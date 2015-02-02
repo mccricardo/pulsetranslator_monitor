bc30fa78f2cff46c3e852967371e01cc55753ccf
Ricardo Castro <mcc.ricardo@gmail.com>
2015-02-02 17:37:02 +0000

    Boostrap monitor.py

---
+++ monitor.py
@@ -0,0 +1,12 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# Default RabbitMQ host settings are as defined in the accompanying
+# vagrant puppet files.
+DEFAULT_RABBIT_HOST = 'localhost'
+DEFAULT_RABBIT_PORT = 5672
+DEFAULT_RABBIT_SSL = False
+DEFAULT_RABBIT_VHOST = '/'
+DEFAULT_RABBIT_USER = 'pulse'
+DEFAULT_RABBIT_PASSWORD = 'pulse'
